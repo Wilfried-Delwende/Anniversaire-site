f1cc00f6fda20ce37c999167cba8a4adc5434d58
Wilfried-Delwende <delwendewill@gmail.com>
2026-08-06 04:00:55 +0000

    Add Firebase and Cloudinary configuration
    Mon studio de dépôt

---
+++ config.js
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDsb6jLUi8We48e_GjXBGe5rRDQ9J0hXJM",
+  authDomain: "anniversaire-site.firebaseapp.com",
+  projectId: "anniversaire-site",
+  storageBucket: "anniversaire-site.firebasestorage.app",
+  messagingSenderId: "176274853119",
+  appId: "1:176274853119:web:dc21194ae0246cc41b50ca"
+};
+
+export const cloudinaryConfig = {
+  cloudName: "adpmubt3",
+  uploadPreset: "anniversaire_media"
+};
