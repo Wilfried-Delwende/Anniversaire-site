c1309a91cfd0810eaa8ee28756faec6a716e0b49
Wilfried-Delwende <delwendewill@gmail.com>
2026-08-25 20:41:52 +0000

    Add salle5Data with multimedia content and descriptions

--- src/data/salle5Data.js
+++ src/data/salle5Data.js
@@ -1,1 +1,190 @@
-
+export const salle5Data = {
+  titre: "Nous",
+  introOurs: "Waouh une histoire. Tu peux me la lire ?",
+  outroOurs: "Ce livre est sûrement le meilleur que j'ai pû lire, il est plein de pur moment et de joie. Sniff... Je peux plus me contrôler sniff sniff c'est... C'est magnifique !!! 😭😭😭",
+  pages: [
+    {
+      id: 1,
+      typeMedia: "photo",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/image/upload/v1786906715/anniversaire/salle-5/30a13673-29c0-4fa5-b6c3-53ab642d9ad4-1_all_35790_klgf0n.jpg",
+      description: "Toujours concentrée sur son téléphone en train de causer avec ses gars 🤣",
+      repliqueOurs: "Tu es très concentrée 🤣"
+    },
+    {
+      id: 2,
+      typeMedia: "photo",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/image/upload/v1786908454/anniversaire/salle-5/1000952579_jpecev.jpg",
+      description: "Qu'est-ce que j'ai dit toujours sur son téléphone avec ses maries 😌\n\nJoyeux anniversaire l'enfant guerrier 🥳 que Dieu te bénisse et te guide pour nous sortir encore de ces visés de qualité 🤌🏾✨. T'es tellement la star que tu penses être et n'oublie pas que t'es copines sont là pour toi ❤️✨\n\n  KOUTIEBOU Céleste Nadine.",
+      repliqueOurs: "Tu aimes vraiment ton téléphone"
+    },
+    {
+      id: 3,
+      typeMedia: "photo",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/image/upload/v1786907416/anniversaire/salle-5/1000557298_ouvx87.jpg",
+      description: "Mais souris un peu non 🤣",
+      repliqueOurs: "Tu avais faim ou quoi 😂"
+    },
+    {
+      id: 4,
+      typeMedia: "photo",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/image/upload/v1786908947/anniversaire/salle-5/30a13673-29c0-4fa5-b6c3-53ab642d9ad4-1_all_36257_ehz5at.jpg",
+      description: "J'ai dit de sourire mais pas à ce point là 🤣",
+      repliqueOurs: "On dirait que ton sourire donne de la joie a une certaine personne 😏"
+    },
+    {
+      id: 5,
+      typeMedia: "video",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/video/upload/v1786909324/anniversaire/salle-5/1000684847_hnyt6b.mp4",
+      description: "Elle t'as fait quoi seulement 🤣",
+      repliqueOurs: "On dirait que tu aimes la bagarre dès 😂"
+    },
+    {
+      id: 6,
+      typeMedia: "video",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/video/upload/v1786909885/anniversaire/salle-5/30a13673-29c0-4fa5-b6c3-53ab642d9ad4-1_all_36248_ol45od.mp4",
+      description: "Que des beaux souvenirs 🥲",
+      repliqueOurs: "La joie rien qu'un beau moment 🤧"
+    },
+    {
+      id: 7,
+      typeMedia: "video",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/video/upload/v1786910685/anniversaire/salle-5/30a13673-29c0-4fa5-b6c3-53ab642d9ad4-1_all_35789_llq7ng.mp4",
+      description: "On faisait quoi même 😭",
+      repliqueOurs: "Vous étiez en train de faire quoi 🤣"
+    },
+    {
+      id: 8,
+      typeMedia: "video",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/video/upload/v1786910958/anniversaire/salle-5/1000626789_sddh7f.mp4",
+      description: "J'ai vraiment apprécié la visite et passer du temps avec toi et vous tous ❤️❤️",
+      repliqueOurs: "Ashley et moi on dirait un chien et un chat en ce moment 😂"
+    },
+    {
+      id: 9,
+      typeMedia: "photo",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/image/upload/v1786911535/anniversaire/salle-5/1000684866_swtcov.jpg",
+      description: "Tu illumine ma vie comme le soleil en plein midi 🤣",
+      repliqueOurs: "Waouh là tu brilles fort tel une déesse 😂"
+    },
+    {
+      id: 10,
+      typeMedia: "photo",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/image/upload/v1786915280/anniversaire/salle-5/1000952748_budwxl.jpg",
+      description: "Elisée s'il ne fait pas attention je risque de le 🔪👀 un jour",
+      repliqueOurs: "On dirait que le gars sur la photo ne vivra pas longtemps s'il ne prend pas ses distances 👀"
+    },
+    {
+      id: 11,
+      typeMedia: "photo",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/image/upload/v1787273206/anniversaire/salle-5/1000950232_z3pb1a.jpg",
+      description: "J'attends toujours ma surprise 🥲",
+      repliqueOurs: "Crois moi ce gars ne va pas s'en sortir comme ça surtout si l'attrape 😑😒. Il se prend pour qui pour escroquer ma Reine 👸🏽"
+    },
+    {
+      id: 12,
+      typeMedia: "photo",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/image/upload/v1787273726/anniversaire/salle-5/1000950233_h7zl41.jpg",
+      description: "J'aime quand tu m'envoies pleins de messages et que tu me racontes ta vie ❤️",
+      repliqueOurs: "Waouh que j'aimerais être à la place de ton mari il est si chanceux 🤧"
+    },
+    {
+      id: 13,
+      typeMedia: "photo",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/image/upload/v1787273918/anniversaire/salle-5/1000950238_noxei0.jpg",
+      description: "Tu doutais de mes talents de poète 😂",
+      repliqueOurs: "Un vrai Shakespeare ce gars 😂 en même temps Shakespeare se nomme William qui est un dérivé de Wilfried donc par substitution c'est vraiment Shakespeare en mode Wilfried Shakespeare 🤣🤣🤣"
+    },
+    {
+      id: 14,
+      typeMedia: "photo",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/image/upload/v1787274382/anniversaire/salle-5/1000950254_tv7yur.jpg",
+      description: "Je ne me lasse pas de ton nom complet 🥲❤️",
+      repliqueOurs: "Waouh que des beaux prénoms. Mon préféré est Noom-Wend 🥲"
+    },
+    {
+      id: 15,
+      typeMedia: "photo",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/image/upload/v1787274609/anniversaire/salle-5/1000950250_ptf2bl.jpg",
+      description: "Ah côté nourriture je vais te gâter. Il faut que tu sois en bonne santé 🥲",
+      repliqueOurs: "Il a raison la nourriture est vitale pour l'organisme. Il faut manger matin midi et soir 5 fruits et légumes et non seulement des mangues 😌🥭"
+    },
+    {
+      id: 16,
+      typeMedia: "photo",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/image/upload/v1787274844/anniversaire/salle-5/1000950251_wu8dcb.jpg",
+      description: "Tu croyais pouvoir te protéger comme ça 😂",
+      repliqueOurs: "Je confirme ça ne va pas t'aider toute résistance est vaine rends toi et commence à manger 😂"
+    },
+    {
+      id: 17,
+      typeMedia: "photo",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/image/upload/v1787275195/anniversaire/salle-5/1000950252_hzoxk0.jpg",
+      description: "Mon pull j'aurais vraiment aimé te le laisser garder non seulement il te va à merveille mais tu es la première et la seule fille a être partie avec un de mes habits et tu en as bien pris soin. Quand je l'ai récupéré j'étais content parce qu'il était avec toi mais triste parce qu'il ne sera plus avec toi mais t'inquiètes 🥲❤️❤️",
+      repliqueOurs: null // Pas de réplique d'ours pour cette page dans le document
+    },
+    {
+      id: 18,
+      typeMedia: "photo",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/image/upload/v1787275490/anniversaire/salle-5/1000665976_yqqspp.jpg",
+      description: "Crois moi je déteste vraiment quand tu me dis de ne pas m'inquiéter quand je sens que tu as un soucis je ne te demande pas de venir demander mon aide pour le résoudre mais simplement me dire ce qui ne va pas et comment tu vas le résoudre. Je t'aime sincèrement mais au delà de là j'aimerais être ton confident que tu aies confiance en moi comme j'ai confiance en toi d'accord ? 🥺❤️",
+      repliqueOurs: "Là ce que je peux dire c'est un conseil même si tu ne veux pas le déranger avec tes soucis que tu crois être pas important tu as quand même des amies sur qui tu peux compter. Ashley ne te laissera jamais tomber ça c'est sûr elle sera toujours là pour toi ❤️"
+    },
+    {
+      id: 19,
+      typeMedia: "photo",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/image/upload/v1787275807/anniversaire/salle-5/1000950234_rg0ps4.jpg",
+      description: "L'un des plus beaux jours de ma vie, la première fois que tu m'as dit que tu m'aimes je n'oublierai jamais 🤧❤️",
+      repliqueOurs: "Le premier mot d'amour comme c'est touchant plein d'émotions c'est magnifique ✨✨"
+    },
+    {
+      id: 20,
+      typeMedia: "video",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/video/upload/v1787276254/anniversaire/salle-5/1000959192_fmcrhf.mp4",
+      description: "Tu étais contente ce jour là et ça m'a rendu content aussi 🙂✨❤️",
+      repliqueOurs: "Là il a raison ton sourire est magnifique et contagieux ✨💓"
+    },
+    {
+      id: 21,
+      typeMedia: "video",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/video/upload/v1787276443/anniversaire/salle-5/1000959190_mcf1jc.mp4",
+      description: "La complicité que tu as avec Ashley c'est magnifique ne la perd jamais pour rien au monde même pas pour un mésentente entre vous ✨✨",
+      repliqueOurs: "Une belle amitié à entretenir pour la vie entière"
+    },
+    {
+      id: 22,
+      typeMedia: "video",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/video/upload/v1787276699/anniversaire/salle-5/1000959191_nqponh.mp4",
+      description: "Une autre belle complicité à entretenir. Je dirais simplement ne laisse rien te séparer de tes copines pas même moi ou un autre garçon.",
+      repliqueOurs: "Tout a déjà été dit reste attaché à tes copines elle seront toujours là pour toi 💓"
+    },
+    {
+      id: 23,
+      typeMedia: "video",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/video/upload/v1787276868/anniversaire/salle-5/1000959197_eospsr.mp4",
+      description: "Nous aurons le temps de faire plein de photos et vidéos ensemble pour l'instant je chéri le peu qu'il y a 💗💓",
+      repliqueOurs: "L'importance n'est pas le nombre de médias que vous avez ensemble mais plutôt les moments passés ensemble ✨"
+    },
+    {
+      id: 24,
+      typeMedia: "video",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/video/upload/v1787277066/anniversaire/salle-5/1000660899_nctt7n.mp4",
+      description: "Tu dis ne pas savoir chanter et pourtant je trouve que tu as une belle voix je dirais plutôt que c'est la timidité qui t'empêche de chanter",
+      repliqueOurs: "Entouka quand il s'agit de TRK tu chantes bien 🤣"
+    },
+    {
+      id: 25,
+      typeMedia: "photo",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/image/upload/v1787278819/anniversaire/salle-5/1000660823_b1gaqh.jpg",
+      description: "Une photo de nous deux ensemble je ne m'en lasse pas jamais ❤️",
+      repliqueOurs: "Waouh rien de tel qu'une photo ensemble pour symboliser votre amour c'est magnifique ✨✨❤️"
+    },
+    {
+      id: 26,
+      typeMedia: "video",
+      urlMedia: "https://res.cloudinary.com/adpmubt3/video/upload/v1787277311/anniversaire/salle-5/1000959309_qa5oug.mp4",
+      description: "Je le répète mais ta beauté devrait être un crime parfait je t'aime à l'infini et au delà bien sûr et je ne cesserai d'essayer de te combler de bonheur 💗💓💞💞❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️ +∞\nDes cœurs à l'infini pour celle qui n'est rien d'autre que la dérivée de mon âme dont la limite vers l'infini est indéterminée. Mathématiquement parlant mon amour pour toi est une fonction sans asymptotes. \nPhysiquement, tu représentes la loi qui dicte mon essence. Chimiquement, je ne suis composé que de molécules d'Ana. \nEn SVT (on ne peut pas dire SVTiquement dès 🤣) mon code génétique se trouve dans ton cœur. \nEn littérature tu es ma source d'inspiration, un modèle de langage parfait digne des plus grandes oeuvres écrites. \nEn sport tu es le fruit de la douleur d'un entraînement acharné.\nEn Histoire tu représentes la belle histoire inachevée de mon cœur.  \nEn géographie tu es mon point de repère lorsque je suis perdu. \nEn musique je dirais que tu es ma Muse qui a fait jaillir des mélodies et des symphonie digne de la chorale céleste. \nEn philosophie Socrate a dit « tout ce que je sais c'est que je ne sais rien » moi je dirais « tout ce que je sais c'est que je ne ment pas quand je dis que je l'aime d'un amour ignorants »\nSpirituellement, je dirais que tu es la grâce et le don le plus pur que Dieu m'a fait ✨.",
+      repliqueOurs: "Waouh un magnifique poème sûrement fait avec amour et sentiments. Je me rends compte maintenant de la profondeur de son amour pour toi. Tu es spécial et unique pour lui et ça donne envie de pleurer 🤧💓💗"
+    }
+  ]
+};
+  
