610916cc8a791e212460e7b1c7aea306f43fd5e7
Wilfried-Delwende <delwendewill@gmail.com>
2026-08-25 20:20:53 +0000

    Add salle4Data.js with mangue recipes and media

---
+++ src/data/salle4Data.js
@@ -0,0 +1,57 @@
+export const mangueData = [
+  {
+    id: 0,
+    type: "intro",
+    media: null,
+    title: "Le Trésor",
+    description: "La fameuse Mangue d'Or...",
+    bearPrompt: "Nous y voilà, la deuxième salle la plus importante et qui expose un trésor 🪎 👀",
+    bearReaction: "Wouah que c'est magnifique ! Je parie que tu pourrais manger plusieurs mangues comme celle-ci",
+    actionText: "C'est pas fini aller vas y tire sur la corde pour fermer les rideaux"
+  },
+  {
+    id: 1,
+    type: "image",
+    media: "https://res.cloudinary.com/adpmubt3/image/upload/v1787258828/anniversaire/salle-4/1000959146_qlwxn4.jpg",
+    title: "Salade Fraîche mangue et crevettes",
+    description: "Ingrédients :\n1 belle mangue mûre mais ferme\n200g de crevettes décortiquées (déjà cuites)\n1 avocat\nLe jus d'un citron vert, coriandre fraîche, sel, poivre et un filet d'huile d'olive.\n\nPréparation :\nLa découpe : Épluchez la mangue et l'avocat, puis coupez-les en petits cubes réguliers...\nL'assemblage : Dans un grand bol, réunissez les cubes avec les crevettes...\nL'assaisonnement : Ajoutez l'huile d'olive, le sel, le poivre...",
+    bearReaction: "Hum c'est une bonne salade qui peut servir de plat d'entrée. Tu pourrais le faire, le fait d'y penser j'ai déjà l'eau à la bouche 😋",
+    actionText: "C'est pas fini ferme encore les rideaux."
+  },
+  {
+    id: 2,
+    type: "image",
+    media: "https://res.cloudinary.com/adpmubt3/image/upload/v1787259356/anniversaire/salle-4/1000959159_hyrsdw.jpg",
+    title: "Mousse légère à la mangue",
+    description: "Ingrédients :\n2 mangues bien mûres\n20cl de crème liquide entière\n2 cuillères à soupe de sucre glace\n\nPréparation :\nLe coulis : Mixez finement la chair des mangues...\nLa chantilly : À l'aide d'un batteur électrique, montez la crème liquide...\nLe mélange : Incorporez la purée de mangue...\nLe repos : Répartissez la mousse dans des verrines...",
+    bearReaction: "Délicieux 😋 rien de tel pour un dessert qu'en penses-tu 🥲",
+    actionText: "C'est pas fini, les images c'est bien mais une vidéo explicative est meilleure alors ferme les rideaux pour laisser apparaître la suite"
+  },
+  {
+    id: 3,
+    type: "video",
+    media: "https://res.cloudinary.com/adpmubt3/video/upload/v1787259648/anniversaire/salle-4/1000959052_eljipg.mp4",
+    title: "Tasses de mousse à la mangue",
+    description: "",
+    bearReaction: "Waouh c'est sûrement un régal 😋",
+    actionText: "Encore un autre. Tu sais quoi faire 👀"
+  },
+  {
+    id: 4,
+    type: "video",
+    media: "https://res.cloudinary.com/adpmubt3/video/upload/v1787260048/anniversaire/salle-4/1000959108_rnriph.mp4",
+    title: "Un délicieux gâteau à la mousse de mangues 🍮🥮",
+    description: "",
+    bearReaction: "Le gâteau!!!!!! Ça fait aucun doute qu'il est délicieux, mais il serait plus savoureux si c'était toi qui l'avais fait (peut être pour une certaine personne 😏)",
+    actionText: "Il reste encore une recette vas y dévoile le vite 🤤🥺"
+  },
+  {
+    id: 5,
+    type: "video",
+    media: "https://res.cloudinary.com/adpmubt3/video/upload/v1787260347/anniversaire/salle-4/1000959140_v5dnc1.webm",
+    title: "Un rafraîchissant smoothie à la mangue",
+    description: "",
+    bearReaction: "Il n'y a rien de tel qu'un smoothie à la mangue facile à faire pour se rafraîchir à tous moment 😋🥲",
+    actionText: "Que dirait tu ensuite de jouer à un jeu"
+  }
+];
