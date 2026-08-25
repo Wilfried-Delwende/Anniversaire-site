6fe083439c7e6fb9a61ed4151833e7097cf9edad
Wilfried-Delwende <delwendewill@gmail.com>
2026-08-25 17:22:06 +0000

    Create App component with preparation message

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function App() {
+  return <div style={{ color: 'white', textAlign: 'center', marginTop: '50px' }}><h1>Site en préparation... 🎉</h1></div>
+}
