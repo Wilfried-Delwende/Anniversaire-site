3487c81998b86a9b8ba9cfa58a6350d5efe8719d
Wilfried-Delwende <delwendewill@gmail.com>
2026-08-26 06:50:17 +0000

    Add salle0Data with clues for user interaction
    Added an array of objects containing clues for a game or interactive experience.

--- src/data/salle0Data.js
+++ src/data/salle0Data.js
@@ -1,1 +1,23 @@
+export const salle0Data = [
+  {
+    id: "bloc-1",
+    contenu: "Hey toi j'ai quelque chose à te montrer, défile vers la droite et pour t'aider un premier indice : un lieu où est exposé des œuvres artistiques, culturelles et de grandes valeurs. Si tu veux en savoir plus continue d'avancer 😌 👉🏾"
+  },
+  {
+    id: "bloc-2",
+    contenu: "avance Encore . Pour te motiver voilà un autre indice : de la musique, des énigmes, un fruit spécial et pleins d'autres encore 🤭. Si tu veux en découvrir plus continue 😉"
+  },
+  {
+    id: "bloc-3",
+    contenu: "Te voilà à mis chemin. J'espère que tu n'es pas fatiguée ça ne fait que commencer. Pour te donner la force de continuer voici le troisième indice : il y a plein de salles chacune d'elle différente et unique mais combien il y en a ? Mystère !!! 🤫🤭"
+  },
+  {
+    id: "bloc-4",
+    contenu: "Tu y es presque encore quelques efforts. Voici quelque chose pour t'accompagner vers la suite. Quatrième indice : un animal possédant un museau avec de grosses narines ( les narines ne te rappelles pas quelque chose ? 😃) maladroit et un peu agité mais c'est qui ? Tu le découvrira bientôt 😄😁😌"
+  },
+  {
+    id: "bloc-5",
+    contenu: "Tu y es arrivé enfin. Il ne te reste plus qu'à traverser la porte pour découvrir un nouveau monde mais avant ce moment, dernier indice : Un cadeau spécial pour un jour spécial le tout pour une personne extraordinaire 🥲. J'espère que les autres Indices ont pu éclairer ta lanterne ? Dans le cas contraire un autre le fera 😌😏🤭 . Bref assez bavardé, ouvre la porte pour faire face à ton destin jeune fille !!! 👀"
+  }
+];
 
