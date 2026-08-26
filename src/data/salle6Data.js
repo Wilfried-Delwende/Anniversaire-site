9181f8577fd0ef57d560cb71d411a5c1e0b6b424
Wilfried-Delwende <delwendewill@gmail.com>
2026-08-26 03:04:15 +0000

    Add birthday messages and dialogues to salle6Data

--- src/data/salle6Data.js
+++ src/data/salle6Data.js
@@ -1,1 +1,27 @@
+// salle6Data.js
+
+export const oursDialogues = [
+  "Je t'ai fait venir ici pour te dire que tes narines ont enfin trouvé une place 😌",
+  "On arrive à la fin de cette expérience et la dernière chose que je voudrais te dire c'est que cette salle représente en faite tout ce qui obsédait mes pensées jusqu'à ce que je fasse ta connaissance. Je t'ai trouvé belle au début et je me disais il y a aucune chance pour moi c'est mort mais j'ai eu le courage de faire le premier pas et j'en suis fière maintenant.\n\nCette salle est pour te dire que tu es devenue le centre de mon univers tu occupes la même place voir une position supérieure à ma passion mon désir de la science. Mon amour pour toi est égal à celle que j'ai pour l'univers et la science c'est à dire infini. Encore joyeux anniversaire à toi, que le Seigneur continue de t'accompagner et de t'aider à mieux grandir dans les commandements qu'il nous a donné. 💓💗💞"
+];
+
+export const cartesVoeux = [
+  {
+    id: 'adora',
+    expediteur: 'Adora',
+    message: "Joyeux anniversaire ma voisine que Dieu te protège et qu'il t'accorde plus d'anniversaire 🤧❤️."
+  },
+  {
+    id: 'nadine',
+    expediteur: 'KOUTIEBOU Céleste Nadine',
+    message: "Joyeux anniversaire l'enfant guerrier 🥳 que Dieu te bénisse et te guide pour nous sortir encore de ces visés de qualité 🤌🏾✨. T'es tellement la star que tu penses être et n'oublie pas que t'es copines sont là pour toi ❤️✨"
+  },
+  {
+    id: 'ashley',
+    expediteur: 'ADHA Marcella Ashley',
+    message: "Ma Nana 💕💕 aujourd'hui c'est un jour spécial pour toi et moi parce que c'est le jour de ta naissance 🥹 [...] on va aller visé les gens l'année prochaine encore n'est ce pas ?🤣🤣... Bref heureux anniversaire à toi Ana ❤️🩷🥰"
+  }
+];
+
+export const messageFinal = "Je t'aime de tout mon être et c'est avec toi que je veux me marier réellement (mais pas pour l'instant je dois d'abord m'assurer que tu auras une belle vie 🥲) \n\n Signé ton mari KAFANDO DELWENDE WILFRIED";
 
