c86a604eb43c633a9ff2261d72a77791896df976
Wilfried-Delwende <delwendewill@gmail.com>
2026-08-26 07:01:49 +0000

    Add salle1Data.js with messages and birthday wishes

---
+++ src/data/salle1Data.js
@@ -0,0 +1,15 @@
+export const messagesOurs = {
+  1: "Salut, s'il te plaît tire sur la ficelle pour allumer la lampe", //[span_0](start_span)[span_0](end_span)
+  2: "C'est pas comme ça que l'on tire. Tire plus fort", //[span_1](start_span)[span_1](end_span)
+  3: "Décidément il faut tout faire pour toi quoi", //[span_2](start_span)[span_2](end_span)
+  4: "Tu vois ? À toi maintenant", //[span_3](start_span)[span_3](end_span)
+  5: "Super ! Tu vois c'était facile .", //[span_4](start_span)[span_4](end_span)
+  6: "Huh ? Mais qu'est-ce qui se passe ? Attends je sors pour", //[span_5](start_span)[span_5](end_span)
+  7: "Aaaaah !", //[span_6](start_span)[span_6](end_span)
+  9: "Ma surprise te plaît ? Et bien ce n'est pas encore terminé ." //[span_7](start_span)[span_7](end_span)
+};
+
+export const messageAnniversaire = {
+  titre: "Joyeux anniversaire Ma Myly 🥳🎉🎊", //[span_8](start_span)[span_8](end_span)
+  texte: "En ce jour spécial je te souhaite une excellente Santé, la joie et le bonheur durable, la prospérité, le Succès au Baccalauréat à venir , de réussir dans tous ce que tu entreprendras et que le Seigneur soit ton guide tout au long de ta vie 💫✨❤️" //[span_9](start_span)[span_9](end_span)
+};
