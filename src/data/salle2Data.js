c9ae981be417040b5c4ea5fc92e391bf133c3ce3
Wilfried-Delwende <delwendewill@gmail.com>
2026-08-26 07:05:35 +0000

    Add salle2Data.js with media and dialogue exports

---
+++ src/data/salle2Data.js
@@ -0,0 +1,7 @@
+export const mediasSalle2 = {
+  audioTransition: "https://res.cloudinary.com/adpmubt3/video/upload/v1786730430/anniversaire/salle-2/TRK_-_COMMENT__VISUALIZER__256k_kc5klo.mp3", //[span_2](start_span)[span_2](end_span)
+  audioHappyBirthday: "https://res.cloudinary.com/adpmubt3/video/upload/v1786739752/anniversaire/salle-2/Happy_birthday_song_x4qnmr.mp3", //[span_3](start_span)[span_3](end_span)
+  videoOursEnvol: "https://res.cloudinary.com/adpmubt3/video/upload/v1786739954/anniversaire/salle-2/1000660899_zaujj5.mp4" //[span_4](start_span)[span_4](end_span)
+};
+
+export const repliqueOurs = "J'ai entendu dire que tu avais quelques notions en piano. Et si jouait une mélodie."; //[span_5](start_span)[span_5](end_span)
